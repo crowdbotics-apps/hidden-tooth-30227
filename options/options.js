5d20cc48748c0108831129450cf10703b83dd5e6
Crowdbotics <team@crowdbotics.com>
2021-08-31 18:41:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "hidden_tooth_30227Identifier",
+  url: "https://hidden_tooth_30227Identifier.botics.co",
+  api: "https://hidden_tooth_30227Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
